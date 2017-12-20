11b8ad3c4d3cb9779eb39a90b3688bd89946909f
jordan gomes <jordasg@gmail.com>
2017-12-20 17:53:35 -0200

    fixed categories order

--- src/categories.js
+++ src/categories.js
@@ -1,7 +1,7 @@
 // this document declares what will be queried to the API and rendered to the categories picker (first item of each array, string) and which details are going to be extracted from the request (second item, array of strings);
 
 const categories = [
-  ["planets", ["climate", "terrain", "population", "films"]],
+  ["planets", ["population", "climate", "terrain", "films"]],
   ["people", ["eye_color", "height", "films"]]
 ];
 
